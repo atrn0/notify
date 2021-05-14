21fa60c33c7a465b594337ed3e733de62dce2049
atrn0 <14242642+aratasato@users.noreply.github.com>
2021-05-14 22:16:22 +0900

    fetch traffic
    (cherry picked from commit fb29055e5042d6b0ef2f702d5e6e4c9db8adfc1c)

--- index.ts
+++ index.ts
@@ -1,5 +1,52 @@
 import schedule from "node-schedule"
+import axios from "axios"
+import { JSDOM } from "jsdom"
 
-const job = schedule.scheduleJob("*/5 * * * * *", function () {
-  console.log("hello!")
-})
+async function fetchTraffic(): Promise<string> {
+  try {
+    const res = await axios.get(
+      "https://sslbb.excite.co.jp/bbhikari/fit/traffic/",
+      {
+        headers: {
+          Cookie: process.env.BBEXCITE_TRAFFIC_COOKIE || "",
+        },
+      }
+    )
+    return res.data
+  } catch (err) {
+    return err
+  }
+}
+
+function parseTraffic(data: string): string {
+  const { document } = new JSDOM(data).window
+  const totalAmountInt = document.getElementsByClassName(
+    "traffic-total-amount-number-int"
+  )[0]?.textContent
+  const totalAmountDecimal = document.getElementsByClassName(
+    "traffic-total-amount-number-decimal"
+  )[0]?.textContent
+
+  if (!totalAmountInt || !totalAmountDecimal) {
+    return "failed to fetch traffic"
+  }
+
+  return totalAmountInt + totalAmountDecimal + "GB"
+}
+
+// const job = schedule.scheduleJob("0 * * * * *", function () {})
+
+async function main() {
+  let data
+  try {
+    data = await fetchTraffic()
+  } catch (err) {
+    console.error(err)
+    return
+  }
+
+  const traffic = parseTraffic(data)
+  console.log(traffic)
+}
+
+main()
